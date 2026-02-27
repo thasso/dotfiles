164769095cbd895ce0c5579bd3edb1f06c4be23b
Thasso Griebel <thasso.griebel@gmail.com>
2026-02-27 10:26:31 +0100

    Do not constantly report that an lsp is checking

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -2,6 +2,16 @@ return {
   "folke/noice.nvim",
   event = "UIEnter",
   opts = {
+    routes = {
+      {
+        filter = {
+          event = "lsp",
+          kind = "progress",
+          find = "[Cc]hecking document",
+        },
+        opts = { skip = true },
+      },
+    },
     lsp = {
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
